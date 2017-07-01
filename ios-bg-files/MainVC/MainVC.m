1a320211ef666b21930214a25422d205552106e0
Michael Kapelko <kornerr@gmail.com>
2017-07-01 08:07:08 -0700

    Add old UI to MainVC

--- ios-bg-files/MainVC/MainVC.m
+++ ios-bg-files/MainVC/MainVC.m
@@ -3,6 +3,11 @@
 
 @interface MainVC ()
 
+@property (nonatomic, strong) IBOutlet UIButton *locationButton;
+@property (nonatomic, strong) IBOutlet UIButton *notificationButton;
+@property (nonatomic, strong) IBOutlet UIButton *directoryButton;
+@property (nonatomic, strong) IBOutlet UILabel *noteLabel;
+
 @end
 
 @implementation MainVC
