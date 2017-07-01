503acfea7ec13866d5e3340ebddc4cce08276738
kornerr <kornerr@kornerrs-imac.local>
2017-07-01 01:15:52 -0700

    Initial Commit

---
+++ ios-bg-files/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios-bg-files
+//
+//  Created by kornerr on 7/1/17.
+//  Copyright © 2017 kornerr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios-bg-files/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios-bg-files
+//
+//  Created by kornerr on 7/1/17.
+//  Copyright © 2017 kornerr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
